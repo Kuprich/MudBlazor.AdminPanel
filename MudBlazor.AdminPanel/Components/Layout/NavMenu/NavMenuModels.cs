32836ce9c590f01930fd50628f659d2cfc81284c
Kupriyanov Ilya <ilya-kyp@yandex.ru>
2024-02-08 15:56:39 +0300

    nav menu structure

---
+++ MudBlazor.AdminPanel/Components/Layout/NavMenu/NavMenuModels.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+
+namespace MudBlazor.AdminPanel.Components.Layout.NavMenu;
+
+public record NavItemModel(string? Href = null,
+    string? Title = null,
+    string? Icon = null,
+    NavLinkMatch Match = NavLinkMatch.Prefix, 
+    List<NavItemModel>? NavItems = null);
+public record NavGropModel(string? Title, List<NavItemModel> NavItems);
+
